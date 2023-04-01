2bfc92fc2ec0283f1abc522953da97920740750a
cpontrelli <cpontrelli@equilar.com>
2023-04-01 15:40:17 -0700

    added lend and borrow transactions

---
+++ contracts/Token.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor() ERC20("MyToken", "MTK") {
+        }
+}
--- scripts/Lend.ts
+++ scripts/Lend.ts
@@ -1,22 +1,32 @@
 import { ethers } from "hardhat";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { IPool__factory } from "../typechain-types";
+import { IPool__factory, Token__factory } from "../typechain-types";
 
-const POOL_ADDRESS = '0x6060Cf73C79098D32c9b936F4B26283427f1BFAd'
+const POOL_ADDRESS = '0x7b5C526B7F8dfdff278b4a3e045083FBA4028790';
+const DAI_ADDRESS = '0xBa8DCeD3512925e52FE67b1b5329187589072A55';
+const USCD_ADDRESS = '0x65aFADD39029741B3b8f0756952C74678c9cEC93';
+const IMPERSONATE_ADDRESS = '0xBA549d2730210AD373f9A29Ae918D3Bc7550C480';
 
 async function main() {
-    let account1: SignerWithAddress;
-    let account2: SignerWithAddress;
-    let account3: SignerWithAddress;
-
-    [account1, account2, account3] = await ethers.getSigners();
-
-    const poolContract = IPool__factory.connect(POOL_ADDRESS, account1);
     
-    const accountData = await poolContract.getUserAccountData(account1.address);
-    
-    console.log(`Total Collateral Base: ${accountData.totalCollateralBase}`);
+    const impersonatedSigner = await ethers.getImpersonatedSigner(IMPERSONATE_ADDRESS);
 
+    const poolContract = IPool__factory.connect(POOL_ADDRESS, impersonatedSigner);
+    const USDCContract = Token__factory.connect(USCD_ADDRESS, impersonatedSigner);
+
+    const approveTx = await USDCContract.approve(POOL_ADDRESS, ethers.utils.parseUnits('1', 6));
+    approveTx.wait();
+  
+    const lendTx = await poolContract.supply(USCD_ADDRESS, ethers.utils.parseUnits('1', 6), impersonatedSigner.address, 0);
+    lendTx.wait();
+
+    let accountData = await poolContract.getUserAccountData(impersonatedSigner.address);
+    console.log(`Total Available Borrow Base: ${accountData.availableBorrowsBase}`);
+
+    const borrowTX = await poolContract.borrow(DAI_ADDRESS, ethers.utils.parseUnits('0.5', 18), 2, 0, impersonatedSigner.address);
+    borrowTX.wait()
+
+    accountData = await poolContract.getUserAccountData(impersonatedSigner.address);
+    console.log(`Total Available Borrow Base: ${accountData.availableBorrowsBase}`);
 }
 
 main().catch((error)=> {
